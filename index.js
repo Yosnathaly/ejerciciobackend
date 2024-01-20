f7850c7b239a95202b52635b9ada60c6d1676ddf
yoskary80@gmail.com <yoskary80@gmail.com>
2024-01-20 14:50:20 -0300

    Clase Backend

---
+++ index.js
@@ -0,0 +1,15 @@
+const namee = 'Juan Perez';
+console.log(namee)
+
+let number = 18;
+console.log(number)
+
+const saludar= (namee) => {
+    console.log(`Hola ${namee}`)
+}
+saludar(namee)
+const suma = (a, b) => {
+    return a + b;
+}
+console.log(suma(2, 3))
+console.log(suma(70, 40))
